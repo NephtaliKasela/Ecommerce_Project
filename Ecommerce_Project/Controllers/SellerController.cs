23d3e74f6a98e29d0c5e10eef4899402b325dc12
Nephtali Kasela <nephtalikasela04@gmail.com>
2024-09-17 15:10:58 +0200

    Add Seller Registration action and view

---
+++ Ecommerce_Project/Controllers/SellerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Project.Controllers
+{
+    public class SellerController : Controller
+    {
+        public IActionResult SellerRegistration()
+        {
+            return View();
+        }
+    }
+}
