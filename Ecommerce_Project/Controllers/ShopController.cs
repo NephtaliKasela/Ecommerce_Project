49d228c18839f2ac8e3313b2228a36aefc038731
Nephtali Kasela <nephtalikasela04@gmail.com>
2024-10-23 15:50:30 +0200

    Add more info to order details view and  shop view

---
+++ Ecommerce_Project/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce_Project.DTOs.ModelViews;
+using Ecommerce_Project.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Project.Controllers
+{
+    public class ShopController : Controller
+    {
+        public readonly IProductService _productService;
+
+        public ShopController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProducts();
+            var v = new Shop_ModelView();
+            v.Products = products.Data;
+            return View(v);
+        }
+    }
+}
---
+++ Ecommerce_Project/DTOs/ModelViews/Shop_ModelView.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce_Project.DTOs.Product;
+
+namespace Ecommerce_Project.DTOs.ModelViews
+{
+    public class Shop_ModelView
+    {
+        public List<GetProductDTO> Products { get; set; }
+
+        public Shop_ModelView()
+        {
+            Products = new List<GetProductDTO>();
+        }
+    }
+}
